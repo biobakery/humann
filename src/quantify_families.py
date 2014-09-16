e5b0016031f8b5b6e9baa41ab40424e39b9de8a7
Lauren McIver <Lauren McIver@localhost>
2014-09-16 14:23:33 -0600

    Order gene families with the most hits first

--- src/quantify_families.py
+++ src/quantify_families.py
@@ -12,13 +12,22 @@ def gene_families(alignments):
     file_handle = open(config.genefamilies_file, "w")
 
     file_handle.write("Gene Family"+config.output_file_column_delimiter+"Hits\n")
+    
     # Compute hits by gene family
+    gene_scores={}
+    gene_output_lines={}
     for gene in alignments.gene_list():
         hit_list=alignments.hits_for_gene(gene)
         total_hits_for_gene=len(hit_list)
         
-        file_handle.write(gene+config.output_file_column_delimiter+
-            str(total_hits_for_gene)+"\n")
+        if gene in gene_scores:
+            gene_scores[gene]+=total_hits_for_gene
+            gene_output_lines[gene].append(gene+config.output_file_column_delimiter+
+            str(total_hits_for_gene))
+        else:
+            gene_scores[gene]=total_hits_for_gene
+            gene_output_lines[gene]=[gene+config.output_file_column_delimiter+
+            str(total_hits_for_gene)]
         
         # merge hits with the same bug
         hits_by_bug={}
@@ -29,10 +38,14 @@ def gene_families(alignments):
             else:
                 hits_by_bug[bug]=1
         
-        # print the hits by bug for this gene
+        # record the hits by bug for this gene
         for bug in hits_by_bug.keys():
-            file_handle.write(gene+config.output_file_category_delimiter+
-                bug+config.output_file_column_delimiter+str(hits_by_bug[bug])+"\n")
+            gene_output_lines[gene].append(gene+config.output_file_category_delimiter+
+                bug+config.output_file_column_delimiter+str(hits_by_bug[bug]))
+        
+    # Write the scores ordered with the top first
+    for gene in sorted(gene_scores, key=gene_scores.get, reverse=True):
+        file_handle.write("\n".join(gene_output_lines[gene])+"\n")    
         
     file_handle.close()
 
