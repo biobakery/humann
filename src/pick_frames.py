4bc6abc5312fa6893e94afffd4f546d6169e80f2
franzosa <franzosa@localhost>
2014-12-17 10:57:33 -0500

    pick_frames.py no longer depends on biopython

--- src/pick_frames.py
+++ src/pick_frames.py
@@ -1,38 +1,110 @@
-#!/usr/bin/env python
+#! /usr/bin/env python
 
 """
-I/O pipe for converting DNA reads to peptides.
-Ignores any reading frame from the read with a
-non-amino acid character (notably "*" = STOP)
+Standalone pipe for converting DNA reads in FASTA
+format to FASTA formatted peptides.
+
+Examines all 6 reading frames and prints those that
+(1) do not contain a stop codon and 
+(2) do not contain a non-amino acid character (here, "X")
 
 Usage: cat reads.fasta | python <this>
 -----------------------------------------------
 Author: Eric Franzosa (eric.franzosa@gmail.com)
 """
 
-import sys, re
-from Bio import SeqIO
+import sys
 
+# ---------------------------------------------------------------
 # constants
-bad_aa_pattern = "[^ACDEFGHIKLMNPQRSTVWY]"
+# ---------------------------------------------------------------
 
-# read fasta dna from stdin / write fasta protein to stdout
-for record in SeqIO.parse( sys.stdin, "fasta" ):
-    header = str( record.name )
-    frames = []
-    # 3 translations from the forward direction
+# used when we find an untranslatable codon
+bad_aa_char = "X"
+
+# for making reverse complement
+switch = {"A":"T", "T":"A", "G":"C", "C":"G"}
+
+# for translating dna to protein
+genetic_code = """
+Ala/A GCT GCC GCA GCG
+Arg/R CGT CGC CGA CGG AGA AGG
+Asn/N AAT AAC
+Asp/D GAT GAC
+Cys/C TGT TGC
+Gln/Q CAA CAG
+Glu/E GAA GAG
+Gly/G GGT GGC GGA GGG
+His/H CAT CAC
+Ile/I ATT ATC ATA
+Leu/L TTA TTG CTT CTC CTA CTG
+Lys/K AAA AAG
+Met/M ATG
+Phe/F TTT TTC
+Pro/P CCT CCC CCA CCG
+Ser/S TCT TCC TCA TCG AGT AGC
+Thr/T ACT ACC ACA ACG
+Trp/W TGG
+Tyr/Y TAT TAC
+Val/V GTT GTC GTA GTG
+Stp/* TAA TGA TAG
+"""
+
+# convert genetic code to dict
+decode = {}
+for line in genetic_code.split( "\n" ):
+    items = line.split( )
+    if len( items ) > 0:
+        long, short = items[0].split( "/" )
+        for codon in items[1:]:
+            decode[codon] = short
+
+# ---------------------------------------------------------------
+# utilities
+# ---------------------------------------------------------------
+        
+def reverse_complement ( dna ):
+    return "".join( [switch[nuc] for nuc in dna[::-1]] )
+    
+def translate ( dna, frame=0 ):
+    peptide = ""
+    i = frame
+    while i + 3 <= len( dna ):
+        peptide += decode.get( dna[i:i+3], bad_aa_char )
+        i += 3
+    return peptide
+
+def pick_frames ( header, sequence ):
+    sequence_rc = reverse_complement( sequence )
+    peptides = []
+    # forward translations
     for i in range( 3 ):
-        frames.append( ["+%d" % ( i ), str( record.seq[i:].translate() )] )
-    # 3 translations from the reverse direction
+        peptides.append( translate( sequence, frame=i ) )
+    # reverse translations
     for i in range( 3 ):
-        frames.append( ["-%d" % ( i ), str( record.seq.reverse_complement()[i:].translate() )] )
-    # check for valid peptides
-    for index, seq in frames:
-        if not re.search( bad_aa_pattern, seq ):
-            """
-            # this option would append the reading frame to the read name
-            e.g. ">read123" -> ">read123_+0", ">read123_+1", etc.
-            print ">%s_%s" % ( header, index )
-            """
-            print ">%s" % ( header )
-            print seq
+        peptides.append( translate( sequence_rc, frame=i ) )
+    for peptide in peptides:
+        if not ( "*" in peptide or bad_aa_char in peptide ):
+            print header
+            print peptide
+
+# ---------------------------------------------------------------
+# main
+# ---------------------------------------------------------------
+
+def main ( ):
+    header, sequence = None, ""
+    for line in sys.stdin:
+        line = line.strip()
+        if line[0] == ">":
+            # new record, process active record and reset
+            if header is not None:
+                pick_frames( header, sequence )
+            header, sequence = line, ""
+        else:
+            sequence += line
+    # process last record
+    pick_frames( header, sequence )
+
+if __name__ == "__main__":
+    main()
