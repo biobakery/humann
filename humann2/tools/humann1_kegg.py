d2c1ccca8930ce792cbd106a3e2ff32cb3d3a4c3
George_Weingart <George_Weingart@localhost>
2015-03-31 16:15:58 -0400

    Tools: Modified theKegg ID Mapping so that it tries to read first the KeggOrgId2OrgNameTable.txt file located in data/pathways - if that fails,  we try to read directly from the kegg website

--- humann2/tools/humann1_kegg.py
+++ humann2/tools/humann1_kegg.py
@@ -3,8 +3,7 @@ from cStringIO import StringIO
 import sys,string
 import sys, os
 import argparse
-from bs4 import BeautifulSoup
-import urllib2
+
 
 
 #********************************************************************************************
@@ -27,9 +26,15 @@ import urllib2
 #  C. kegg file with bug names: http://www.genome.jp/kegg/catalog/org_list.html             *
 #                                                                                           *
 #  The program is invoked as follows:                                                       *
-#  python humann1_kegg.py --igenels ./data/genels   --ikoc  ./data/koc --ikeggtrans  "http://www.genome.jp/kegg/catalog/org_list.html"     --o output_extract
+#  python humann1_kegg.py --igenels ./data/genels   --ikoc  ./data/koc --ikeggtrans  "http://www.genome.jp/kegg/catalog/org_list.html"  
+#              --ikeggOrgId2OrgName    ../data/pathways/KeggOrgId2OrgNameTable.txt   --o output_extract
 #                                                                                            *
-# Written by George Weingart  March 20, 2015   george.weingart@gmail.com                     *
+#
+#  The program tries to read the Kegg translation table OrgId -->OrgName that is located in  *
+#  ../data/pathways/KeggOrgId2OrgNameTable.txt                                               *
+#  And if that fails,  it tries to get that info from Kegg itself ( "http://www.genome.jp/kegg/catalog/org_list.html" )
+#                                                                                            *
+# Written by George Weingart  March 31, 2015   george.weingart@gmail.com                     *
 #********************************************************************************************
 
 
@@ -43,12 +48,60 @@ def read_params(x):
 	CommonArea = dict()	
 	parser = argparse.ArgumentParser(description='Humann1_kegg extract generator')
 	parser.add_argument('--igenels', action="store", dest='igenels',nargs='?')
-	parser.add_argument('--ikeggtrans', action="store", dest='ikeggtrans',nargs='?')
+	parser.add_argument('--ikeggtrans', action="store", dest='ikeggtrans',nargs='?', default="http://www.genome.jp/kegg/catalog/org_list.html")
 	parser.add_argument('--ikoc', action="store", dest='ikoc',nargs='?')
+	parser.add_argument('--ikeggOrgId2OrgName', action="store", dest='ikeggOrgId2OrgName',nargs='?')
 	parser.add_argument('--o', action="store", dest='o',nargs='?')
 	CommonArea['parser'] = parser
 	return  CommonArea
 
+	
+
+#************************************************************************
+#*   Load the dictionary of OrgId --> OrgName                           *
+#* The routine takes its data from the following web-site               *
+#*  "http://www.genome.jp/kegg/catalog/org_list.html"                   *
+#************************************************************************
+def  ReadHtmlKegTable(CommonArea):
+		print "Trying to read the online kegg html table to translate OrgId to OrgName : http://www.genome.jp/kegg/catalog/org_list.html"
+		from bs4 import BeautifulSoup
+		import urllib2
+		
+		dOrgIdOrgName = dict()
+		f1  =  urllib2.urlopen(CommonArea['ikeggtrans'])
+		soup1 = BeautifulSoup(f1)
+
+		for child in soup1.recursiveChildGenerator():
+				name = getattr(child, "name", None)
+				try:
+					xchild = str(child)
+					xname = str(name)
+					if xname.startswith("a")  and xchild.startswith('<a href="/dbget-bin/www_'):
+						ychild = str(xchild)
+						OrgName = ychild.split(">")[1].split('<')[0]
+						OrgNameOrgId = OrgId + "\t" +  OrgName 
+						dOrgIdOrgName[OrgId] = OrgName
+						
+					if xname.startswith("a")     and xchild.startswith('<a href="/kegg-bin/show_organism?org='):
+						ychild = str(xchild)
+						OrgId = ychild.split(">")[1].split('<')[0]
+				except:
+					pass
+		CommonArea['dOrgIdOrgName'] = dOrgIdOrgName 
+		return CommonArea 
+
+
+
+#************************************************************************
+#*  Try to read the Sequential File                                     *
+#************************************************************************
+def  ReadSequentialTranslationFile(CommonArea):
+		CommonArea['ikeggOrgId2OrgName_file'] = open(CommonArea['ikeggOrgId2OrgName'])
+		for iKeggOrgLine in CommonArea['ikeggOrgId2OrgName_file']: 
+			OrgId = iKeggOrgLine.split("\t")[0]
+			OrgName = iKeggOrgLine.split("\t")[1].rstrip('\n')
+			CommonArea['dOrgIdOrgName'][OrgId]  = OrgName 
+		return CommonArea 
 
 #*************************************************************************************
 #*  Main Program                                                                     *
@@ -70,41 +123,18 @@ CommonArea['ikeggtrans'] = results.ikeggtrans
 CommonArea['oFile'] = results.o
 OutputFile = open(CommonArea['oFile'],'w')
 
-
-
-#************************************************************************
-#*   Load the dictionary of OrgId --> OrgName                           *
-#* The routine takes its data from the following web-site               *
-#*  "http://www.genome.jp/kegg/catalog/org_list.html"                   *
-#************************************************************************
-f1  =  urllib2.urlopen(CommonArea['ikeggtrans'])
-soup1 = BeautifulSoup(f1)
-
-dOrgIdOrgName = dict()
-for child in soup1.recursiveChildGenerator():
-		name = getattr(child, "name", None)
-		try:
-			xchild = str(child)
-			xname = str(name)
-			if xname.startswith("a")  and xchild.startswith('<a href="/dbget-bin/www_'):
-				ychild = str(xchild)
-				OrgName = ychild.split(">")[1].split('<')[0]
-				OrgNameOrgId = OrgId + "\t" +  OrgName 
-				dOrgIdOrgName[OrgId] = OrgName
-				
-				
-				
-			if xname.startswith("a")     and xchild.startswith('<a href="/kegg-bin/show_organism?org='):
-				ychild = str(xchild)
-				OrgId = ychild.split(">")[1].split('<')[0]
-		except:
-			pass
+CommonArea['ikeggOrgId2OrgName'] = results.ikeggOrgId2OrgName
 
 
 
+
+CommonArea['dOrgIdOrgName'] = dict()
+try:
+	CommonArea  =  ReadSequentialTranslationFile(CommonArea)   #First,  try to read the sequential file in our directory
+except:
+	print "Reading the Table OrgId--> Name in ../data/pathways/KeggOrgId2OrgNameTable.txt Failed"
+	CommonArea  =  ReadHtmlKegTable(CommonArea)   #But if that fails - try directly from Kegg
  
-
-
 
 dBugGeneToKO = dict()
 #************************************************************************
@@ -131,7 +161,7 @@ for iLine in CommonArea['igenels_file']:
 	iGeneLengthNucleotide = 3 * iGeneLengthAA 
 	iBugName = "n/a"
 	try:
-		iBugName = dOrgIdOrgName[iBug]
+		iBugName = CommonArea['dOrgIdOrgName'][iBug]
 	except:
 		iBugName = iBug
 	
@@ -143,7 +173,7 @@ for iLine in CommonArea['igenels_file']:
 		
 	
 	try:
-		OrgName = dOrgIdOrgName[iBug] 
+		OrgName = CommonArea['dOrgIdOrgName'][iBug] 
 	except: 
 		OrgName = iBug	
 	OutputLine = iBug + ":" + iGene + "\t" + KOEntry + \
