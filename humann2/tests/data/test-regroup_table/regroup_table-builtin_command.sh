a3f5527684f6663eadc90a281341f52da673b632
franzosa <franzosa@localhost>
2015-08-26 16:10:03 -0400

    added test files and commands for eric scripts

---
+++ humann2/tests/data/test-regroup_table/regroup_table-builtin_command.sh
@@ -0,0 +1,4 @@
+# command
+humann2_regroup_table -i regroup_table-builtin_input.txt -g uniref50_rxn -o regroup_table-builtin_output.txt
+# output
+# 1) regroup_table-builtin_output.txt
---
+++ humann2/tests/data/test-rna_dna_norm/rna_dna_norm-command.sh
@@ -0,0 +1,6 @@
+# command
+humann2_rna_dna_norm -d rna_dna_norm-dna.txt -r rna_dna_norm-rna.txt -o rna_dna_norm
+# output:
+# 1) rna_dna_norm-smoothed_dna.txt
+# 2) rna_dna_norm-smoothed_rna.txt
+# 1) rna_dna_norm-relative_expression.txt
