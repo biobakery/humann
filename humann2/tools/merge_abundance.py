a6c61de901722f6b560ba88358424566e4fe20e2
Lauren McIver <Lauren McIver@localhost>
2015-10-05 17:52:09 -0600

    merge abundance script: allow for ec abundance input files

--- humann2/tools/merge_abundance.py
+++ humann2/tools/merge_abundance.py
@@ -88,11 +88,10 @@ def merge_abundances(gene_table,pathways_to_genes,input_pathways,output,addition
                     if bug in gene_table[gene]:
                         # check if additional information should be added to the gene
                         gene_with_info=gene
-                        if additional_gene_info:
-                            if gene in additional_gene_info:
-                                gene_with_info+=GENE_NAME_DELIMITER+additional_gene_info[gene]
+                        if gene in additional_gene_info:
+                            gene_with_info+=GENE_NAME_DELIMITER+additional_gene_info[gene]
                         # print out the gene for this bug
-                        output_line=TABLE_DELIMITER.join([BUG_DELIMITER.join([pathway,bug,gene_with_info]),gene_table[gene][bug]])
+                        output_line=TABLE_DELIMITER.join([BUG_DELIMITER.join([pathway,bug,gene_with_info]),str(gene_table[gene][bug])])
                         write_file_handle.write(output_line+"\n")
     write_file_handle.close()
     
@@ -103,7 +102,7 @@ def read_mapping(gene_mapping_file,pathway_mapping_file):
     
     # read the gene to reaction mappings
     reactions_to_genes={} 
-    genes_to_ecs={}
+    reactions_to_ecs={}
     try:
         if gene_mapping_file.endswith(".gz"):
             file_handle=gzip.open(gene_mapping_file)
@@ -118,19 +117,13 @@ def read_mapping(gene_mapping_file,pathway_mapping_file):
         reaction=data.pop(0)
         ecs=data.pop(0).split(EC_DELIMITER)
         reactions_to_genes[reaction]=data
-        for gene in data:
-            if not gene in genes_to_ecs:
-                genes_to_ecs[gene]=set()
-            genes_to_ecs[gene].update(ecs)
+        reactions_to_ecs[reaction]=ecs
                 
     file_handle.close()
     
-    # convert the genes to ecs from sets to strings
-    for gene in genes_to_ecs:
-        genes_to_ecs[gene]=EC_DELIMITER.join(list(genes_to_ecs[gene]))
-    
     # read the reaction to pathway mappings
     pathways_to_genes={}
+    pathways_to_ecs={}
     try:
         file_handle=open(pathway_mapping_file)
     except EnvironmentError:
@@ -142,13 +135,16 @@ def read_mapping(gene_mapping_file,pathway_mapping_file):
             pathway, structure = info
             # get the reactions from the structure
             genes=set()
+            ecs=set()
             for item in structure.split(" "):
                 genes.update(reactions_to_genes.get(item,[]))
+                ecs.update(reactions_to_ecs.get(item,[]))
             pathways_to_genes[pathway]=genes
+            pathways_to_ecs[pathway]=ecs
         
     file_handle.close()
   
-    return pathways_to_genes,genes_to_ecs
+    return pathways_to_genes,pathways_to_ecs
         
 def read_gene_table(gene_table):
     """
@@ -172,20 +168,44 @@ def read_gene_table(gene_table):
                     if len(info) > 1 :
                         gene = info[0]
                         gene_names[gene]=" ".join(info[1:])
-                data_point=data[1]
-            except IndexError:
+                data_point=float(data[1])
+            except (IndexError,ValueError):
                 gene=""
 
-            if gene:
-                if not gene in gene_table_data:
-                    gene_table_data[gene]={}
-                gene_table_data[gene][bug]=data_point
+            if gene and data_point > 0:
+                # check for an EC set
+                genes=[gene]
+                if EC_DELIMITER in gene:
+                    genes=gene.split(EC_DELIMITER)
+                    
+                for gene in genes:
+                    if not gene in gene_table_data:
+                        gene_table_data[gene]={}
+                    gene_table_data[gene][bug]=gene_table_data[gene].get(bug,0)+data_point
 
         line=file_handle.readline()
             
     file_handle.close()
         
     return gene_table_data, gene_names
+
+def determine_mapping_type(gene_table,pathways_to_genes,pathways_to_ecs):
+    """
+    Determine if the input file is of gene families or EC abundance type
+    """
+    
+    all_genes=set()
+    all_ecs=set()
+    for pathway,genes in pathways_to_genes.iteritems():
+        all_genes.update(genes)
+        all_ecs.update(pathways_to_ecs[pathway])
+    
+    for gene in gene_table:
+        if gene in all_genes:
+            return pathways_to_genes
+        elif gene in all_ecs:
+            return pathways_to_ecs
+    
 
 def parse_arguments(args):
     """ 
@@ -197,16 +217,12 @@ def parse_arguments(args):
         formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument(
         "--input-genes",
-        help="the gene family abundance file\n",
+        help="the gene family or EC abundance file\n",
         required=True)
     parser.add_argument(
         "--input-pathways",
         help="the pathway abundance file\n",
         required=True)
-    parser.add_argument(
-        "-a","--add",
-        help="add gene information\n",
-        choices=["ec","name"])
     parser.add_argument(
         "--gene-mapping",
         help="gene family to reaction mapping file\n")
@@ -254,16 +270,12 @@ def main():
         args.pathway_mapping=config.metacyc_reactions_to_pathways
     
     print("Reading the gene to pathway mapping.")
-    pathways_to_genes,genes_to_ecs=read_mapping(args.gene_mapping,args.pathway_mapping)
+    pathways_to_genes,pathways_to_ecs=read_mapping(args.gene_mapping,args.pathway_mapping)
     
-    additional_info=None
-    if args.add == "name":
-        additional_info=gene_names
-    elif args.add == "ec":
-        additional_info=genes_to_ecs
+    pathways_to_input=determine_mapping_type(gene_table, pathways_to_genes, pathways_to_ecs)
     
     print("Merging abundances.")
-    merge_abundances(gene_table,pathways_to_genes,input_pathways,output,additional_info)
+    merge_abundances(gene_table,pathways_to_input,input_pathways,output,gene_names)
     
 
 if __name__ == "__main__":
