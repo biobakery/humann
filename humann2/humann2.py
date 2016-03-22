3cd28719f0828a773d456447c83512a9c806a1f4
Lauren McIver <Lauren McIver@localhost>
2016-03-22 16:19:05 -0600

    Update to new version

--- humann2/humann2.py
+++ humann2/humann2.py
@@ -65,7 +65,7 @@ from .quantify import modules
 # name global logging instance
 logger=logging.getLogger(__name__)
 
-VERSION="0.6.2"
+VERSION="0.7.0"
 MAX_SIZE_DEMO_INPUT_FILE=10
 
 def parse_arguments(args):
--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ import tempfile
 import re
 import time
 
-VERSION = "0.6.2"
+VERSION = "0.7.0"
 AUTHOR = "HUMAnN2 Development Team"
 AUTHOR_EMAIL = "humann-users@googlegroups.com"
 MAINTAINER = "Lauren McIver"
