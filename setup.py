16d18b8842d7c267f1a6a2dff1b970c82d8d3f9a
Lauren McIver <Lauren McIver@localhost>
2016-04-22 14:14:43 -0600

    Add tool to create genus level gene families file

--- setup.py
+++ setup.py
@@ -593,7 +593,8 @@ setuptools.setup(
             'humann2_merge_abundance_tables = humann2.tools.merge_abundance:main',
             'humann2_blastx_coverage = humann2.search.blastx_coverage:main',
             'humann2_test = humann2.tests.humann2_test:main',
-            'humann2_build_custom_database = humann2.tools.build_custom_database:main'
+            'humann2_build_custom_database = humann2.tools.build_custom_database:main',
+            'humann2_genefamilies_genus_level = humann2.tools.genefamilies_genus_level:main'
         ]},
     test_suite= 'humann2.tests.humann2_test.unittests_suite_only',
     zip_safe = False
