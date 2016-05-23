fc27213110298d3fad0cf6446b2d1760259ed8bd
franzosa <franzosa@localhost>
2016-05-23 12:43:33 -0400

    <unistrat.py> renamed <infer_taxonomy.py>

--- humann2/tools/infer_taxonomy.py
+++ humann2/tools/infer_taxonomy.py
@@ -6,11 +6,31 @@ import csv
 import argparse
 import util
 
+description = """
+HUMAnN2 utility for inferring "unclassified" taxonomy
+=====================================================
+Based on the lowest common ancestor (LCA) annotation
+of each UniRef50/90 cluster, infer approximate taxonomy 
+for unclassified features at a target level of resolution 
+(default=Family). Will modify features of known genus/species 
+to match target level.
+
+Requires the following supplmental data files:
+
+uniref50-tol-lca.dat.gz (for UniRef50)
+uniref90-tol-lca.dat.gz (for UniRef90)
+
+Which can be downloaded from:
+https://bitbucket.org/biobakery/humann2/src/tip/humann2/data/misc/
+"""
+
 # ---------------------------------------------------------------
 # constants
 # ---------------------------------------------------------------
 
+c_root = "Root"
 c_levels = [
+    c_root,
     "Kingdom",
     "Phylum",
     "Class",
@@ -23,6 +43,7 @@ c_umode = "unclassified"
 c_smode = "stratified"
 c_tol_header = "# TOL"
 c_lca_header = "# LCA"
+c_unclassified = "unclassified"
 
 # ---------------------------------------------------------------
 # helper objects
@@ -37,7 +58,7 @@ class Taxon:
 class TreeOfLife:
     def __init__( self ):
         self.nodes = {}
-        self.root = Taxon( "ROOT", "ROOT", None )
+        self.root = Taxon( c_root, c_root, None )
         self.nodes[self.root.name] = self.root
     def attach( self, node ):
         if node.name not in self.nodes:
@@ -57,29 +78,33 @@ class TreeOfLife:
 # argument parsing
 # ---------------------------------------------------------------
 
-def get_args( ):
-    parser = argparse.ArgumentParser()
+def get_args ():
+    """ Get args from Argparse """
+    parser = argparse.ArgumentParser(
+        description=description,
+        formatter_class=argparse.RawTextHelpFormatter
+        )
     parser.add_argument( "-i", "--input", 
                          required=True,
                          help="HUMAnN2 output table" )
     parser.add_argument( "-o", "--output", 
                          default=None,
-                         help="Destination for modified table" )
+                         help="Destination for modified table; default=STDOUT" )
     parser.add_argument( "-l", "--level",
                          choices=c_levels,
                          default="Family",
-                         help="Desired level for taxonomic estimation/summation" )
+                         help="Desired level for taxonomic estimation/summation; default=Family" )
     parser.add_argument( "-d", "--datafile", 
                          default="uniref50-tol-lca.dat.gz", 
-                         help="Location of the uniref(50/90)-tol-lca data file" )
+                         help="Location of the uniref(50/90)-tol-lca data file; default=<HERE>" )
     parser.add_argument( "-m", "--mode", 
                          choices=[c_tmode, c_umode, c_smode],
                          default=c_tmode,
-                         help="Which rows to include in the estimation/summation" )
+                         help="Which rows to include in the estimation/summation; default=TOTALS" )
     parser.add_argument( "-t", "--threshold", 
                          type=float, 
-                         default=0, 
-                         help="Minimum frequency for a new taxon to be included" )
+                         default=1e-4, 
+                         help="Minimum frequency for a new taxon to be included; default=1e-4" )
     args = parser.parse_args()
     return args
 
@@ -97,13 +122,14 @@ def build_taxmap( features, target_rank, p_datafile ):
     tol_mode = False
     lca_mode = False
     with util.try_zip_open( p_datafile ) as fh:
+        print( "Loadnig taxonomic data from "+p_datafile, file=sys.stderr )
         for row in csv.reader( fh, csv.excel_tab ):
             if row[0] == c_tol_header:
-                print( "Loading tree of life from: {}".format( p_datafile ), file=sys.stderr )
+                print( "  Loading TOL data", file=sys.stderr )
                 tol_mode = True
                 continue
             if row[0] == c_lca_header:
-                print( "Loading UniRef LCAs from: {}".format( p_datafile ), file=sys.stderr )
+                print( "  Loading LCA data", file=sys.stderr )
                 tol_mode = False
                 lca_mode = True
                 continue
@@ -135,10 +161,10 @@ def build_taxmap( features, target_rank, p_datafile ):
 def tax_connect( feature, taxmap ):
     old = feature
     feature, name, stratum = util.fsplit( feature )
-    if stratum is None or stratum == "unclassified":
-        stratum2 = taxmap.get( feature, "unclassified" )
+    if stratum is None or stratum == c_unclassified:
+        stratum2 = taxmap.get( feature, c_unclassified )
     else:
-        stratum2 = taxmap.get( stratum, "unclassified" )
+        stratum2 = taxmap.get( stratum, c_unclassified )
     return util.fjoin( feature, name, stratum2 )
 
 # ---------------------------------------------------------------
@@ -162,13 +188,16 @@ def main( ):
     for i, rowhead in enumerate( tbl.rowheads ):
         feature, name, stratum = util.fsplit( rowhead )
         new_rowhead = tax_connect( rowhead, taxmap )
+        # unmapped is never stratified
+        if feature == util.c_unmapped:
+            index.setdefault( rowhead, [] ).append( i )
         # outside of unclassfied mode, keep totals
         if stratum is None and args.mode != c_umode:
             index.setdefault( rowhead, [] ).append( i )
             # in totals mode, guess at taxnomy from uniref name
             if args.mode == c_tmode:
                 index.setdefault( new_rowhead, [] ).append( i )
-        elif stratum == "unclassified" and args.mode == c_umode:
+        elif stratum == c_unclassified and args.mode == c_umode:
             # in unclassified mode, make a new row for the total...
             index.setdefault( util.fjoin( feature, name, None ), [] ).append( i )
             # ...then replace "unclassified" with inferred taxonomy
@@ -186,6 +215,22 @@ def main( ):
         data2.append( newrow )
     tbl.rowheads = rowheads2
     tbl.data = data2
+    # report on performance
+    success, total = 0, 0
+    for rowhead in tbl.rowheads:
+        feature, name, stratum = util.fsplit( rowhead )
+        if stratum is not None:
+            total += 1
+            if stratum != c_unclassified:
+                success += 1
+    print( "Of {TOTAL} stratifications, {SUCCESS} mapped at {TARGET} level ({PERCENT}%)".format( 
+            TOTAL=total,
+            SUCCESS=success,
+            TARGET=args.level,
+            PERCENT=round( 100 * success / float( total ), 1 ),
+            ),
+           file=sys.stderr,
+           )
     # output
     tbl.write( args.output, unfloat=True )
 
