bc5528e567f916aafd888eef820125ed36b095e6
Lauren McIver <Lauren McIver@localhost>
2016-09-21 12:43:39 -0600

    Update version number

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ import tempfile
 import re
 import time
 
-VERSION = "0.9.2"
+VERSION = "0.9.3"
 AUTHOR = "HUMAnN2 Development Team"
 AUTHOR_EMAIL = "humann-users@googlegroups.com"
 MAINTAINER = "Lauren McIver"
