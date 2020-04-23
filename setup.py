3f96abff4ce1416a11822a43c052e5df635cc054
ljmciver <lauren.j.mciver@gmail.com>
2020-04-23 18:16:16 +0000

    Change version format for pypi package name

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ import tempfile
 import re
 import time
 
-VERSION = "3.0.0-alpha"
+VERSION = "3.0.0.alpha.0"
 AUTHOR = "HUMAnN2 Development Team"
 AUTHOR_EMAIL = "humann-users@googlegroups.com"
 MAINTAINER = "Lauren McIver"
