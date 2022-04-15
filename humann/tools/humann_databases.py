4c87cca928d71af4834a01aa07686484def729e0
ljmciver <lauren.j.mciver@gmail.com>
2022-04-15 12:26:27 -0400

    Update chocophlan downloads to latest versions

--- humann/tools/humann_databases.py
+++ humann/tools/humann_databases.py
@@ -51,8 +51,8 @@ from .. import utilities
 current_downloads={
     "chocophlan" : 
         {
-            "full" : "http://huttenhower.sph.harvard.edu/humann_data/chocophlan/full_chocophlan.v296_201901b.tar.gz",
-            "DEMO" : "http://huttenhower.sph.harvard.edu/humann_data/chocophlan/DEMO_chocophlan.v296_201901b.tar.gz"
+            "full" : "http://huttenhower.sph.harvard.edu/humann_data/chocophlan/full_chocophlan.v201901_v31.tar.gz",
+            "DEMO" : "http://huttenhower.sph.harvard.edu/humann_data/chocophlan/DEMO_chocophlan.v201901_v31.tar.gz"
         },
     "uniref" : 
         {
